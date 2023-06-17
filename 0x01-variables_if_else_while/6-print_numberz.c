7dc6362a12e834e567e0275aeb441d60630cc07a
BeshoyMansour <beshoymansour96@gmail.com>
2023-06-16 17:57:27 -0700

    6-print_numberz.c

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/* more headers goes there */
+/* betty style doc for function main goes there */
+/**
+ * main - Entry point
+ *
+ * This program checks whether an integer is negative, positive, or zero.
+ *
+ * Return: Always 0 (Success)
+ */
+/* betty style doc for function main goes there */
+int main(void)
+{
+char x;
+for (x = '0'; x <= '9'; x++)
+putchar(x);
+putchar('\n');
+return (0);
+}
