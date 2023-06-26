edbd3fcdaf2247ff209bf5692c58e47876218f87
BeshoyMansour <beshoymansour96@gmail.com>
2023-06-26 15:39:18 -0700

    update

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _puts - Print a string folllowed by a new line.
+ *
+ * @str: String to be printed
+ *
+ * Return: Nothing
+ */
+
+void _puts(char *str)
+{
+int i;
+int i = 0;
+while (*str[i] != '\0')
+_putchar (i0);
+}
