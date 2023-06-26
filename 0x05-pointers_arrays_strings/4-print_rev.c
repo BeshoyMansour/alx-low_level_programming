86c714570039985f4c56749cb396e62a62db4cd4
BeshoyMansour <beshoymansour96@gmail.com>
2023-06-26 16:15:42 -0700

    update

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,23 @@
+#include "main.h"
+#include <stdio.h>
+#include "holberton.h"
+
+/**
+ * print_rev - reverse string
+ *
+ * @s: String to be reversed
+ *
+ * Return: void
+ */
+void print_rev(char *s)
+{
+int x;
+x = 0;
+while (s[x] != '\0')
+x++
+for (x = x - 1 ; x >= 0 ; x--)
+{
+_putchar(s[x]);
+}
+_putchar('\n');
+}
